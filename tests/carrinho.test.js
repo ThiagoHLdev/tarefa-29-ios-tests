8c93607fbf0d2acab70acb802d503f3d2c1d353c
Thiago henrique <goncalvesthiago1132@gmail.com>
2025-09-30 10:03:38 -0300

    Delete tests directory

--- tests/carrinho.test.js
+++
@@ -1,7 +0,0 @@
-describe('Carrinho de compras', () => {
-    it('Deve adicionar produto ao carrinho', async () => {
-        await $('~addToCart').click();
-        await $('~carrinhoTab').click();
-        await expect($('~produtoNoCarrinho')).toBeDisplayed();
-    });
-});
